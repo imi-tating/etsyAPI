c9a1a94613afa7e7119fd9e674467132b09b63f1
Mandi Blanton <imitating@yahoo.com>
2018-08-07 16:48:52 -0700

    fixed SE font-size per lead-dev request

--- app.js
+++ app.js
@@ -207,6 +207,8 @@ function generateKeyValuePairsObject(arrayOfWords) {
     arrayOfObjects.push({ key: word, value: wordsCounted[word] });
   }
 
+
+
   arrayOfObjects.sort(function (a, b) {
     return b.value - a.value
   })
